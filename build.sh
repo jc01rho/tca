c5beb9e29fb7c6d7540efee24e474eceba27401d
whrho <whrho@sparrowfasoo.com>
2021-10-26 19:04:00 +0900

    fixed

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ chmod +x ./context.sh
 ./context.sh
 
 docker pull ubuntu:20.04
-cho TeamCity/webapps > context/.dockerignore
+echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
 docker build -f "generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile" -t teamcity-minimal-agent:local-linux "context"
