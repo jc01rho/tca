db3082ef3e8c97b15dc1d9fe57df4758cd275416
whrho <whrho@sparrowfasoo.com>
2021-10-26 18:59:37 +0900

    fixed

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
-wget -c https://download.jetbrains.com/teamcity/TeamCity-2021.1.4.tar.gz
-tar -zxvf TeamCity-2021.1.4.tar.gz -C context
+set arg1=%1
+
+wget -c https://download.jetbrains.com/teamcity/TeamCity-%arg1%%.tar.gz
+tar -zxvf TeamCity-%arg1%.tar.gz -C context
 
 call generate.cmd
 call context.cmd
@@ -12,5 +14,10 @@ echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
 docker build -f "generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:local-nanoserver-1809 "context"
-docker build -f "generated/windows/Agent/windowsservercore/1809/Dockerfile" -t teamcity-agent:local-windowsservercore-1809 "context"
+docker build -f "generated/windows/Agent/windowsservercore/1809/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest "context"
+
+docker login -u admin -p 1234 192.168.100.123:10301
+docker push 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 
+docker push 192.168.100.123:10301/infra/teamcity-agent-windows:latest
+
 
---
+++ build.sh
@@ -0,0 +1,25 @@
+
+
+wget -c https://download.jetbrains.com/teamcity/TeamCity-$1.tar.gz
+tar -zxvf TeamCity-$1.tar.gz -C context
+
+chmod +x ./generate.sh
+chmod +x ./context.sh
+./generate.sh
+./context.sh
+
+docker pull ubuntu:20.04
+cho TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile" -t teamcity-minimal-agent:local-linux "context"
+echo 2> context/.dockerignore
+echo TeamCity >> context/.dockerignore
+docker build -f "generated/linux/Agent/Ubuntu/20.04/Dockerfile" -t teamcity-agent:local-linux "context"
+docker build -f "generated/linux/Agent/Ubuntu/20.04-sudo/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent:%arg1%-linux-sudo-sparrow-1  -t 192.168.100.123:10301/infra/teamcity-agent:latest "context"
+
+docker login -u admin -p 1234 192.168.100.123:10301
+docker push 192.168.100.123:10301/infra/teamcity-agent:%arg1%-linux-sudo-sparrow-1
+docker push 192.168.100.123:10301/infra/teamcity-agent:latest
+
+
