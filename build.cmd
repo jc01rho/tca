3aed46f7f643df4f09ebdd3cd6b7aaadd0443fca
whrho <whrho@sparrowfasoo.com>
2021-10-27 14:19:00 +0900

    test

--- build.cmd
+++ build.cmd
@@ -16,7 +16,7 @@
 @REM docker build -f "generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:local-nanoserver-1809 "context"
 @REM docker build -f "generated/windows/Agent/windowsservercore/1809/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest "context"
 
-docker build --network host -f "increbuild/windows/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:2021.2-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest .
+docker build --network bridge -f "increbuild/windows/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:2021.2-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest .
 docker login -u admin -p 1234 192.168.100.123:10301
 docker push 192.168.100.123:10301/infra/teamcity-agent-windows:2021.2-sparrow-1
 docker push 192.168.100.123:10301/infra/teamcity-agent-windows:latest
