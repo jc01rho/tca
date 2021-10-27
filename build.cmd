3293cd05a5033403d1ba4322f21259149d71060c
whrho <whrho@sparrowfasoo.com>
2021-10-27 13:58:31 +0900

    fixed

--- build.cmd
+++ build.cmd
@@ -1,23 +1,24 @@
-set arg1=%1
+@REM set arg1=%1
 
-wget -c https://download.jetbrains.com/teamcity/TeamCity-%arg1%.tar.gz
-tar -zxvf TeamCity-%arg1%.tar.gz -C context
+@REM wget -c https://download.jetbrains.com/teamcity/TeamCity-%arg1%.tar.gz
+@REM tar -zxvf TeamCity-%arg1%.tar.gz -C context
 
-call generate.cmd
-call context.cmd
+@REM call generate.cmd
+@REM call context.cmd
 
 
-docker pull mcr.microsoft.com/windows/nanoserver:1809
-docker pull mcr.microsoft.com/powershell:nanoserver-1809
-docker pull mcr.microsoft.com/dotnet/framework/sdk:4.8-windowsservercore-ltsc2019
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:local-nanoserver-1809 "context"
-docker build -f "generated/windows/Agent/windowsservercore/1809/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest "context"
+@REM docker pull mcr.microsoft.com/windows/nanoserver:1809
+@REM docker pull mcr.microsoft.com/powershell:nanoserver-1809
+@REM docker pull mcr.microsoft.com/dotnet/framework/sdk:4.8-windowsservercore-ltsc2019
+@REM echo TeamCity/webapps > context/.dockerignore
+@REM echo TeamCity/devPackage >> context/.dockerignore
+@REM echo TeamCity/lib >> context/.dockerignore
+@REM docker build -f "generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:local-nanoserver-1809 "context"
+@REM docker build -f "generated/windows/Agent/windowsservercore/1809/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest "context"
 
+docker build -f "increbuild/windows/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent-windows:2021.2-sparrow-1 -t 192.168.100.123:10301/infra/teamcity-agent-windows:latest "context"
 docker login -u admin -p 1234 192.168.100.123:10301
-docker push 192.168.100.123:10301/infra/teamcity-agent-windows:%arg1%-sparrow-1 
+docker push 192.168.100.123:10301/infra/teamcity-agent-windows:2021.2-sparrow-1
 docker push 192.168.100.123:10301/infra/teamcity-agent-windows:latest
 
 
--- build.sh
+++ build.sh
@@ -1,25 +1,30 @@
 
 
-wget -c https://download.jetbrains.com/teamcity/TeamCity-$1.tar.gz
-tar -zxvf TeamCity-$1.tar.gz -C context
+# wget -c https://download.jetbrains.com/teamcity/TeamCity-$1.tar.gz
+# tar -zxvf TeamCity-$1.tar.gz -C context
 
-chmod +x ./generate.sh
-chmod +x ./context.sh
-./generate.sh
-./context.sh
+# chmod +x ./generate.sh
+# chmod +x ./context.sh
+# ./generate.sh
+# ./context.sh
 
-docker pull ubuntu:20.04
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile" -t teamcity-minimal-agent:local-linux "context"
-echo 2> context/.dockerignore
-echo TeamCity >> context/.dockerignore
-docker build -f "generated/linux/Agent/Ubuntu/20.04/Dockerfile" -t teamcity-agent:local-linux "context"
-docker build -f "generated/linux/Agent/Ubuntu/20.04-sudo/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent:%arg1%-linux-sudo-sparrow-1  -t 192.168.100.123:10301/infra/teamcity-agent:latest "context"
+# docker pull ubuntu:20.04
+# echo TeamCity/webapps > context/.dockerignore
+# echo TeamCity/devPackage >> context/.dockerignore
+# echo TeamCity/lib >> context/.dockerignore
+# docker build -f "generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile" -t teamcity-minimal-agent:local-linux "context"
+# echo 2> context/.dockerignore
+# echo TeamCity >> context/.dockerignore
+# docker build -f "generated/linux/Agent/Ubuntu/20.04/Dockerfile" -t teamcity-agent:local-linux "context"
+# docker build -f "generated/linux/Agent/Ubuntu/20.04-sudo/Dockerfile" -t 192.168.100.123:10301/infra/teamcity-agent:%arg1%-linux-sudo-sparrow-1  -t 192.168.100.123:10301/infra/teamcity-agent:latest "context"
+
+
+#2021.2
+docker build -f increbuild/linux/Dockerfile -t 192.168.100.123:10301/infra/teamcity-agent:2021.2-linux-sudo-sparrow-1  -t 192.168.100.123:10301/infra/teamcity-agent:latest
+
 
 docker login -u admin -p 1234 192.168.100.123:10301
-docker push 192.168.100.123:10301/infra/teamcity-agent:%arg1%-linux-sudo-sparrow-1
+docker push 192.168.100.123:10301/infra/teamcity-agent:2021.2-linux-sudo-sparrow-1
 docker push 192.168.100.123:10301/infra/teamcity-agent:latest
 
 
