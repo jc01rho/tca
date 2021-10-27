9924a33ff89c4c8c0307b2d952d39cce77cbf959
sparrow-ci-linux2 <sparrow@sparrow-c-linux2.fasoo.com>
2021-10-27 13:33:01 +0900

    fixed

--- tool/sdk.sh
+++ tool/sdk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #-H localhost:2375
-docker run -i --rm -w="/teamcity" -v "$(pwd):/teamcity" mcr.microsoft.com/dotnet/sdk:3.1 $@
+docker run -i --rm -w="/teamcity" -v "$(pwd):/teamcity" mcr.microsoft.com/dotnet/sdk:5.0 $@
